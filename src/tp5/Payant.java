4fe783f81377c113fac5fc431a3b1d7ecc562d05
AUR.WILKOSZ <AUR.WILKOSZ@s124-9.pedago.mermoz.edu>
2019-11-18 16:57:13 +0100

    Début TP5

---
+++ src/tp5/Payant.java
@@ -0,0 +1,5 @@
+package tp5;
+
+public interface Payant {
+	public float getPrix();
+}
---
+++ src/tp5/Service.java
@@ -0,0 +1,10 @@
+package tp5;
+
+public class Service implements Payant {
+	float prix;
+	
+	public float getPrix()
+	{
+		return prix;
+	}
+}
